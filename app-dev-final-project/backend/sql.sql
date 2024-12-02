359fb43f76542661f19cd7915ea09f69a7eeb6c8
James Miller <james.miller.1363@gmail.com>
2024-12-01 22:55:27 -0500

    minor change

--- app-dev-final-project/backend/sql.sql
+++ app-dev-final-project/backend/sql.sql
@@ -20,7 +20,7 @@ CREATE TABLE challenge (
 
 CREATE TABLE photo (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    URL TEXT NOT NULL,
+    FileData BLOB NOT NULL
     Status TEXT CHECK(Status IN ('pending', 'approved', 'denied')) DEFAULT 'pending',
     ChallengeID INTEGER NOT NULL,
     TeamID INTEGER NOT NULL,

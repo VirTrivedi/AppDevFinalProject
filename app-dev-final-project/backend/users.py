a8a9c99b1e1726af90fd919bfdb7f415435d8eea
James Miller <james.miller.1363@gmail.com>
2024-12-01 20:27:18 -0500

    minor database thing

--- app-dev-final-project/backend/users.py
+++ app-dev-final-project/backend/users.py
@@ -1,38 +1,43 @@
-challengesData = [
-    ("Take a picture of your team getting boba", "2024-09-22", "2024-09-28", 50),
-    ("Take a picture of your team meeting in someone's dorm", "2024-09-29", "2024-10-05", 50),
-    ("Take a picture of your team standing in front of a local landmark", "2024-10-06", "2024-10-12", 50),
-    ("Take a picture of your team mimicking the same animal", "2024-10-13", "2024-10-19", 50),
-    ("Take a picture of your team doing a group jump at the same time in an outdoor area", "2024-10-20", "2024-10-26", 50),
-    ("Take a picture of your team wearing their Halloween costumes", "2024-10-27", "2024-11-02", 50),
-    ("Take a picture of your team lying on the ground in a fun shape", "2024-11-03", "2024-11-09", 50),
-    ("Take a picture of your team having a show and tell", "2024-11-10", "2024-11-16", 50),
-    ("Take a picture of your team making a human chain with everyone holding hands", "2024-11-17", "2024-11-23", 50),
-    ("Take a picture of your team wearing the same color", "2024-12-01", "2024-12-07", 50)
-]
+import sqlite3
+
+# Connect to the SQLite database
+con = sqlite3.connect('mentee_chal_data.db')
+cur = con.cursor()
 
 userData = [
-    (1, "Samai Goat", "Samai@terpmail.edu", "Samai", 2000, 'mentee')
-    (2, "Vir Tiredi", "Vir@terpmail.edu", "Vir", 103, 'mentee')
-    (3, "Madeline Moldrem", "Madeline@terpmail.edu", "Madeline", 98, 'mentee')
-    (4, "Sam Tyles", "Sam@terpmail.edu", "Sam", 65, 'mentee')
-    (5, "Riya Lakhani", "Riya@terpmail.edu", "Riya", 55, 'mentee')
-    (6, "James Miller", "James@terpmail.edu", "James", 63, 'mentee')
-    (7, "Kimber Gonzalez Lopez", "Kimber@terpmail.edu", "Kimber", -1, 'admin')
-    (8, "Evelyn Jiang", "Evelyn@terpmail.edu", "Evelyn", -1, 'admin')
-    (9, "Jack Hughes", "Jack@terpmail.edu", "Jack", 135, 'mentee')
-    (10, "Justin Bieber", "Justin@terpmail.edu", "Justin", 43, 'mentee')
-    (11, "Chuck Norris", "Chuck@terpmail.edu", "Chuck", 100, 'mentee')
-    (12, "Matt Gashaw", "Matt@terpmail.edu", "Matt", 843, 'mentee')
-    (13, "Bruce Wayne", "Bruce@terpmail.edu", "Bruce", 21, 'mentee')
-    (14, "Monkey D. Luffy", "Luffy@terpmail.edu", "Luffy", 84, 'mentee')
-    (15, "John Cena", "John@terpmail.edu", "Samai", 0, 'mentee')
-    (16, "Lebron James", "Lebron@terpmail.edu", "Lebron", 100, 'mentee')
-    (17, "Clark Kent", "Clark@terpmail.edu", "Clark", 134, 'mentee')
-    (18, "Sabrina Carpenter", "Sabrina@terpmail.edu", "Sabrina", 24, 'mentee')
-    (19, "Santa Claus", "Santa@terpmail.edu", "Santa", 215, 'mentee')
-    (20, "Buddy The Elf", "Buddy@terpmail.edu", "Buddy", 3, 'mentee')
-    (21, "Luke Skywalker", "Luke@terpmail.edu", "Luke", 12, 'mentee')
-    (22, "Bilbo Baggins", "Bilbo@terpmail.edu", "Bilbo", 34, 'mentee')
-
+    ("Samai Goat", "Samai@terpmail.edu", "Samai", 2000, 'mentee')
+    ("Vir Tiredi", "Vir@terpmail.edu", "Vir", 103, 'mentee')
+    ("Madeline Moldrem", "Madeline@terpmail.edu", "Madeline", 98, 'mentee')
+    ("Sam Tyles", "Sam@terpmail.edu", "Sam", 65, 'mentee')
+    ("Riya Lakhani", "Riya@terpmail.edu", "Riya", 55, 'mentee')
+    ("James Miller", "James@terpmail.edu", "James", 63, 'mentee')
+    ("Kimber Gonzalez Lopez", "Kimber@terpmail.edu", "Kimber", -1, 'admin')
+    ("Evelyn Jiang", "Evelyn@terpmail.edu", "Evelyn", -1, 'admin')
+    ("Jack Hughes", "Jack@terpmail.edu", "Jack", 135, 'mentee')
+    ("Justin Bieber", "Justin@terpmail.edu", "Justin", 43, 'mentee')
+    ("Chuck Norris", "Chuck@terpmail.edu", "Chuck", 100, 'mentee')
+    ("Matt Gashaw", "Matt@terpmail.edu", "Matt", 843, 'mentee')
+    ("Bruce Wayne", "Bruce@terpmail.edu", "Bruce", 21, 'mentee')
+    ("Monkey D. Luffy", "Luffy@terpmail.edu", "Luffy", 84, 'mentee')
+    ("John Cena", "John@terpmail.edu", "Samai", 0, 'mentee')
+    ("Lebron James", "Lebron@terpmail.edu", "Lebron", 100, 'mentee')
+    ("Clark Kent", "Clark@terpmail.edu", "Clark", 134, 'mentee')
+    ("Sabrina Carpenter", "Sabrina@terpmail.edu", "Sabrina", 24, 'mentee')
+    ("Santa Claus", "Santa@terpmail.edu", "Santa", 215, 'mentee')
+    ("Buddy The Elf", "Buddy@terpmail.edu", "Buddy", 3, 'mentee')
+    ("Luke Skywalker", "Luke@terpmail.edu", "Luke", 12, 'mentee')
+    ("Bilbo Baggins", "Bilbo@terpmail.edu", "Bilbo", 34, 'mentee')
 ]
+
+
+# Insert updated user data
+cur.executemany(
+    "INSERT INTO user (Name, Email, Password, Points, Role) VALUES (?,?,?,?,?)",
+    userData
+)
+
+# Commit changes and close the connection
+con.commit()
+con.close()
+
+print("Users updated successfully!")
